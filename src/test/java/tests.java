0791c345b4a2838ee50f6cbe1075f96dbc53a9c7
tavisca-apaithane <apaithane@tavisca.in>
2019-08-14 09:51:06 +0530

    changed Jenkinsfile

---
+++ src/test/java/tests.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class tests {
+
+    @Test
+    public void test1()
+    {
+        assertEquals("abhishek","abhishek");
+    }
+
+}
